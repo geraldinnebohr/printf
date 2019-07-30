592c62e1cfceaee42d4dd1f05e13578e30d9ae90
geraldinnebohr <geraldinnebohr@gmail.com>
2019-07-30 22:36:35 +0000

    [FEATURE #2] code for 1-task

---
+++ 1_functions.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * printf_digit - funtion that prints a digit.
+ * @list: arguments.
+ * Return: 0.
+ */
+int printf_digit(va_list list)
+{
+	int arg = va_arg(list, int);
+	int i;
+
+	i = aux(arg);
+	return (i);
+}
+
+/**
+ * aux - auxiliar recursive function
+ * @arg: is an integer
+ * Return: index
+ */
+int aux(int arg)
+{
+	int i;
+
+	i = 0;
+	if (arg < 0)
+	{
+		_putchar('-');
+		arg = -arg;
+	}
+	if (arg / 10)
+		i += aux(arg /  10);
+
+	_putchar(arg % 10 + '0');
+	i++;
+	return (i);
+}
